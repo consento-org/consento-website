d5678f03dc9ebfc5cab1545252d26bc631f2a550
Nathaniel <nihongotraveler@icloud.com>
2020-02-14 14:36:50 +0800

    Changed topbar.js code. Also, I double checked the SASS file for indentation.

--- assets/main/js/topbar.js
+++ assets/main/js/topbar.js
@@ -1,9 +1,5 @@
 $(window).scroll(function(e) {
     let currentScrollpos = window.pageYOffset;
     let element = document.getElementById("top-bar");
-    if (currentScrollpos > 40) {
-        element.classList.add('scroll_shadow');
-    } else {
-        element.classList.remove('scroll_shadow');
-    };
+    element.classList.toggle('scroll_shadow', currentScrollpos > 40);
 });
