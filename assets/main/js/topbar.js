5ac3a3093b950b4e80fe2595002f1d702d7a9e8d
Martin Heidegger <martin.heidegger@gmail.com>
2020-02-26 22:33:22 +0900

    Using scoped function to make sure that there are no external effects.
    Signed-off-by: Martin Heidegger <martin.heidegger@gmail.com>

--- assets/main/js/topbar.js
+++ assets/main/js/topbar.js
@@ -1,17 +1,18 @@
-const scrollShadow = function(clicked) {
-    let element = document.getElementById("top-bar");
-    if (clicked == true && window.pageYOffset < 40) {
-         element.classList.toggle('scroll_shadow');
-    } else {
-        window.pageYOffset > 40 || document.getElementById('navbarSupportedContent').classList.contains('show') ? element.classList.add('scroll_shadow') : element.classList.remove('scroll_shadow');
+(function () {
+    const scrollShadow = function(clicked) {
+        let element = document.getElementById("top-bar");
+        if (clicked == true && window.pageYOffset < 40) {
+            element.classList.toggle('scroll_shadow');
+        } else {
+            window.pageYOffset > 40 || document.getElementById('navbarSupportedContent').classList.contains('show') ? element.classList.add('scroll_shadow') : element.classList.remove('scroll_shadow');
+        }
     }
-}
 
-document.getElementById('toggle-button').addEventListener("click", function() {
-    scrollShadow(true)
-});
+    document.getElementById('toggle-button').addEventListener("click", function() {
+        scrollShadow(true)
+    });
 
-$(document).ready(scrollShadow);
+    $(document).ready(scrollShadow);
 
-$(window).scroll(scrollShadow);
-
+    $(window).scroll(scrollShadow);
+})()
